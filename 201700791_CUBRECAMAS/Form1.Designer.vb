a73a95f8d721dcfd23e35406861e69529f2af28b
FatimaIMM <89328685+FatimaIMM@users.noreply.github.com>
2021-08-21 21:46:30 -0600

    TareaPreparatoria1

---
+++ 201700791_CUBRECAMAS/Form1.Designer.vb
@@ -0,0 +1,384 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(32, 61)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(61, 17)
+        Me.RadioButton1.TabIndex = 1
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Imperial"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(32, 84)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(78, 17)
+        Me.RadioButton2.TabIndex = 2
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Matrimonial"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(32, 107)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(57, 17)
+        Me.RadioButton3.TabIndex = 3
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Queen"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(32, 130)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(46, 17)
+        Me.RadioButton4.TabIndex = 4
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "King"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(28, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(153, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Tamaño Cubrecama"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(299, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(151, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Material Cubrecama"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(303, 62)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(46, 17)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "Lino"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(303, 84)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(65, 17)
+        Me.CheckBox2.TabIndex = 8
+        Me.CheckBox2.Text = "Algodón"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(303, 107)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(51, 17)
+        Me.CheckBox3.TabIndex = 9
+        Me.CheckBox3.Text = "Seda"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(303, 131)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(74, 17)
+        Me.CheckBox4.TabIndex = 10
+        Me.CheckBox4.Text = "Hilo crudo"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(86, 489)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(190, 489)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Limpiar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(29, 201)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Yardas lino"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(122, 198)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 14
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 232)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 13)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Yardas algodón"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(122, 229)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 16
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(30, 265)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 13)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "Yardas seda"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(122, 262)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 18
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(30, 300)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(89, 13)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "Yardas hilo crudo"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(122, 295)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 20
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(307, 232)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(75, 13)
+        Me.Label8.TabIndex = 22
+        Me.Label8.Text = "Total:     Q 0.0"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(307, 199)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(75, 13)
+        Me.Label9.TabIndex = 23
+        Me.Label9.Text = "Total:     Q 0.0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(307, 264)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(75, 13)
+        Me.Label7.TabIndex = 24
+        Me.Label7.Text = "Total:     Q 0.0"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(307, 296)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(75, 13)
+        Me.Label10.TabIndex = 25
+        Me.Label10.Text = "Total:     Q 0.0"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(133, 367)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(196, 21)
+        Me.Label11.TabIndex = 26
+        Me.Label11.Text = "Precio de costo:     Q 0.0"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(133, 397)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(197, 21)
+        Me.Label12.TabIndex = 27
+        Me.Label12.Text = "Precio de venta:     Q 0.0"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(212, 428)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(117, 21)
+        Me.Label13.TabIndex = 28
+        Me.Label13.Text = "Total:     Q 0.0"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(279, 489)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 29
+        Me.Button3.Text = "Salir"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(502, 546)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ 201700791_CUBRECAMAS/Form1.vb
@@ -0,0 +1,259 @@
+﻿Imports System.Math
+
+Public Class Form1
+
+    Dim precioManoObra As Double = 0.0
+    Dim mensajePrecioCosto As String = "Precio de costo:     Q "
+    Dim mensajePrecioVenta As String = "Precio de venta:     Q "
+    Dim mensajeTotal As String = "Total:     Q "
+    Dim precioMaterialLino As Double = 15
+    Dim precioMaterialAlgodon As Double = 23.99
+    Dim precioMaterialSeda As Double = 30.99
+    Dim precioMaterialHiloCrudo As Double = 39.99
+    Dim opcionSeleccionada As Integer = 0
+    Dim seleccionLino As Boolean
+    Dim seleccionAlgodon As Boolean
+    Dim seleccionSeda As Boolean
+    Dim seleccionHiloCrudo As Boolean
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        precioManoObra = 65.5
+        opcionSeleccionada = 1
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        precioManoObra = 85.99
+        opcionSeleccionada = 2
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        precioManoObra = 99.99
+        opcionSeleccionada = 3
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        precioManoObra = 105.99
+        opcionSeleccionada = 4
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged_1(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+        If CheckBox1.Checked = True Then
+            seleccionLino = True
+        Else
+            seleccionLino = False
+        End If
+
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+
+        If CheckBox2.Checked = True Then
+            seleccionAlgodon = True
+        Else
+            seleccionAlgodon = False
+        End If
+
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+
+        If CheckBox3.Checked = True Then
+            seleccionSeda = True
+        Else
+            seleccionSeda = False
+        End If
+
+    End Sub
+
+    Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox4.CheckedChanged
+
+        If CheckBox4.Checked = True Then
+            seleccionHiloCrudo = True
+        Else
+            seleccionHiloCrudo = False
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim errorRegistroYardas As Boolean = False
+        Dim totalYardas As Single = 0
+        Dim totalPrecioYardas As Single = 0
+        Dim totalPrecioCosto As Single = 0
+
+        If seleccionLino = True Then
+
+            If TextBox1.Text = "" Then
+                errorRegistroYardas = True
+            Else
+                Dim yardaActual = CType(TextBox1.Text, Single)
+                Dim totalPrecioYardaLino = precioMaterialLino * yardaActual
+                totalYardas += yardaActual
+                totalPrecioYardas += totalPrecioYardaLino
+                Label9.Text = mensajeTotal + CType(Round(totalPrecioYardaLino, 2), String)
+            End If
+
+        End If
+
+        If seleccionAlgodon = True Then
+
+            If TextBox2.Text = "" Then
+                errorRegistroYardas = True
+            Else
+                Dim yardaActual = CType(TextBox2.Text, Single)
+                Dim totalPrecioYardaAlgodon = precioMaterialAlgodon * yardaActual
+                totalYardas += yardaActual
+                totalPrecioYardas += totalPrecioYardaAlgodon
+                Label8.Text = mensajeTotal + CType(Round(totalPrecioYardaAlgodon, 2), String)
+            End If
+
+        End If
+
+        If seleccionSeda = True Then
+
+            If TextBox3.Text = "" Then
+                errorRegistroYardas = True
+            Else
+                Dim yardaActual = CType(TextBox3.Text, Single)
+                Dim totalPrecioYardaSeda = precioMaterialSeda * yardaActual
+                totalYardas += yardaActual
+                totalPrecioYardas += totalPrecioYardaSeda
+                Label7.Text = mensajeTotal + CType(Round(totalPrecioYardaSeda, 2), String)
+            End If
+
+        End If
+
+        If seleccionHiloCrudo = True Then
+
+            If TextBox4.Text = "" Then
+                errorRegistroYardas = True
+            Else
+                Dim yardaActual = CType(TextBox4.Text, Single)
+                Dim totalPrecioYardaHiloCrudo = precioMaterialHiloCrudo * yardaActual
+                totalYardas += yardaActual
+                totalPrecioYardas += totalPrecioYardaHiloCrudo
+                Label10.Text = mensajeTotal + CType(Round(totalPrecioYardaHiloCrudo, 2), String)
+            End If
+
+        End If
+
+        If opcionSeleccionada = 1 Then
+
+            If totalYardas = 3 Then
+                totalPrecioCosto = totalPrecioYardas + 65.5
+                Label11.Text = mensajePrecioCosto + CType(Round(totalPrecioCosto, 2), String)
+            Else
+                Label11.Text = mensajePrecioCosto + "0.0"
+                Label12.Text = mensajePrecioVenta + "0.0"
+                Label9.Text = mensajeTotal + "0.0"
+                Label8.Text = mensajeTotal + "0.0"
+                Label7.Text = mensajeTotal + "0.0"
+                Label10.Text = mensajeTotal + "0.0"
+                MessageBox.Show("La cantidad de yardas ingresada es incorrecta.", "Registro de yardas", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                errorRegistroYardas = False
+            End If
+
+        ElseIf opcionSeleccionada = 2 Then
+
+            If totalYardas = 5 Then
+                totalPrecioCosto = totalPrecioYardas + 85.99
+                Label11.Text = mensajePrecioCosto + CType(Round(totalPrecioCosto, 2), String)
+            Else
+                Label11.Text = mensajePrecioCosto + "0.0"
+                Label12.Text = mensajePrecioVenta + "0.0"
+                Label9.Text = mensajeTotal + "0.0"
+                Label8.Text = mensajeTotal + "0.0"
+                Label7.Text = mensajeTotal + "0.0"
+                Label10.Text = mensajeTotal + "0.0"
+                MessageBox.Show("La cantidad de yardas ingresada es incorrecta.", "Registro de yardas", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                errorRegistroYardas = False
+            End If
+
+        ElseIf opcionSeleccionada = 3 Then
+
+            If totalYardas = 6 Then
+                totalPrecioCosto = totalPrecioYardas + 99.99
+                Label11.Text = mensajePrecioCosto + CType(Round(totalPrecioCosto, 2), String)
+            Else
+                Label11.Text = mensajePrecioCosto + "0.0"
+                Label12.Text = mensajePrecioVenta + "0.0"
+                Label9.Text = mensajeTotal + "0.0"
+                Label8.Text = mensajeTotal + "0.0"
+                Label7.Text = mensajeTotal + "0.0"
+                Label10.Text = mensajeTotal + "0.0"
+                MessageBox.Show("La cantidad de yardas ingresada es incorrecta.", "Registro de yardas", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                errorRegistroYardas = False
+            End If
+
+        ElseIf opcionSeleccionada = 4 Then
+
+            If totalYardas = 7 Then
+                totalPrecioCosto = totalPrecioYardas + 105.99
+                Label11.Text = mensajePrecioCosto + CType(Round(totalPrecioCosto, 2), String)
+            Else
+                Label11.Text = mensajePrecioCosto + "0.0"
+                Label12.Text = mensajePrecioVenta + "0.0"
+                Label9.Text = mensajeTotal + "0.0"
+                Label8.Text = mensajeTotal + "0.0"
+                Label7.Text = mensajeTotal + "0.0"
+                Label10.Text = mensajeTotal + "0.0"
+                MessageBox.Show("La cantidad de yardas ingresada es incorrecta.", "Registro de yardas", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                errorRegistroYardas = False
+            End If
+
+        End If
+
+
+        If errorRegistroYardas = True Then
+            MessageBox.Show("No se ha ingresado el total de yardas para todos los productos seleccionados.", "Registro de Yardas", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+
+            Dim precioVenta As Single = 0.65 * totalPrecioCosto
+            Label12.Text = mensajePrecioVenta + CType(Round(precioVenta, 2), String)
+            Label13.Text = mensajeTotal + CType(Round(totalPrecioCosto + precioVenta, 2), String)
+
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+
+        Label9.Text = mensajeTotal + "0.0"
+        Label8.Text = mensajeTotal + "0.0"
+        Label7.Text = mensajeTotal + "0.0"
+        Label10.Text = mensajeTotal + "0.0"
+
+        Label11.Text = mensajePrecioCosto + "0.0"
+        Label12.Text = mensajePrecioVenta + "0.0"
+        Label13.Text = mensajeTotal + "0.0"
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (MsgBox("Desea salir del programa", vbQuestion + vbYesNo, "Salida") = vbYes) Then
+            End
+        Else
+            TextBox1.Clear()
+            TextBox2.Clear()
+            TextBox3.Clear()
+            TextBox4.Clear()
+            TextBox1.Focus()
+            CheckBox1.Checked = False
+            CheckBox2.Checked = False
+            CheckBox3.Checked = False
+            CheckBox4.Checked = False
+            RadioButton1.Checked = False
+            RadioButton2.Checked = False
+            RadioButton3.Checked = False
+            RadioButton4.Checked = False
+        End If
+
+    End Sub
+End Class
